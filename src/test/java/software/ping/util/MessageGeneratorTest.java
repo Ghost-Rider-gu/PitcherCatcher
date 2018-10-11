b79a7e7ead00aa474f4d8748e38817736a094b76
Ghost-Rider-gu <ghostrider.gu@gmail.com>
2018-10-11 10:34:32 +0300

    add tests

--- src/test/java/software/ping/util/MessageGeneratorTest.java
+++ src/test/java/software/ping/util/MessageGeneratorTest.java
@@ -1,16 +1,30 @@
 package software.ping.util;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
+/**
+ * Tests for MessageGenerator class.
+ */
 public class MessageGeneratorTest {
-    @Test
-    public void generateMessage() throws Exception {
+
+    private static MessageGenerator messageGenerator;
+    private static final Integer MESSAGE_SIZE = 200;
+
+    @Before
+    public void setup() {
+        messageGenerator = new MessageGenerator();
     }
 
     @Test
-    public void checkLengthMessage() throws Exception {
+    public void shouldGetNewMessage() throws Exception {
+        String newString = messageGenerator.generateMessage(MESSAGE_SIZE);
+
+        assertNotNull(newString);
+        assertEquals((long)newString.length(), (long)MESSAGE_SIZE);
     }
 
 }
--- src/test/java/software/ping/util/core/PitcherCatcherTest.java
+++ src/test/java/software/ping/util/core/PitcherCatcherTest.java
@@ -1,4 +1,52 @@
 package software.ping.util.core;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import software.ping.core.Catcher;
+import software.ping.core.Pitcher;
+import software.ping.util.CommandLineParams;
+
+/**
+ * Tests for Pitcher and Catcher.
+ */
 public class PitcherCatcherTest {
+
+    private static final Integer PORT = 4550;
+    private static final Integer MESSAGE_SIZE = 200;
+    private static final String BIND_ADDRESS = "127.0.0.1";
+    private static final String HOST = "127.0.0.1";
+    private static final Integer MESSAGES_PER_SECOND = 20;
+
+    private static Pitcher pitcher;
+    private static Catcher catcher;
+
+    @Before
+    public void setup() {
+        CommandLineParams pitcherParams = new CommandLineParams();
+        pitcherParams.setHostName(HOST);
+        pitcherParams.setMessageLength(MESSAGE_SIZE);
+        pitcherParams.setMps(MESSAGES_PER_SECOND);
+        pitcherParams.setPort(PORT);
+
+        CommandLineParams catcherParams = new CommandLineParams();
+        catcherParams.setPort(PORT);
+        catcherParams.setHostName(BIND_ADDRESS);
+
+        pitcher = new Pitcher(pitcherParams);
+        catcher = new Catcher(catcherParams);
+    }
+
+    @After
+    public void release() {
+        catcher.stopCatcher();
+        pitcher.stopPitcher();
+    }
+
+    @Test
+    public void shouldStartPitcherCatcher() throws Exception {
+        catcher.startCatcher();
+        pitcher.startPitcher();
+    }
+
 }
